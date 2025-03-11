e66a5952f85d48c36ac1d23a810646ccef62b2cf
nikhil-mangla <e22cseu0288@bennett.edu.in>
2025-03-11 23:33:00 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,12 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
-import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-export default clerkMiddleware({
-  afterAuth: (auth, req) => {
-    if (auth.userId && auth.isPublicRoute) {
-      let path = "/select-org";
-      if (auth.orgId) {
-        path = `/organization/${auth.orgId}`;
-      }
-      const orgSelection = new URL(path, req.url);
-      return NextResponse.redirect(orgSelection)
-    }
-    if (!auth.userId &&
-        !auth.isPublicRoute) {
-        return RedirectToSignIn({ returnBackUrl: req.url });
-    }
-    if (auth.userId &&
-        !auth.orgId &&
-        req.nextUrl.pathname !== "/select-org") {
-        return NextResponse.redirect(new URL("/select-org", req.url));
-    }
-  },
-});
-
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:css|js|map|json|jpe?g|png|gif|svg|webp|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // ✅ Always run middleware for API routes
-    "/api/:path*",
-    "/trpc/:path*",
-  ],
-};
+  matcher: '/about/:path*',
+}
